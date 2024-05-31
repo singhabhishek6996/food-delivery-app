976aebe1650573616094420e051c53e33f2d4289
Abhishek Singh <singhabhishek6996@gmail.com>
2024-05-31 13:36:53 +0530

    Add server url for client and admin

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ export const StoreContext = createContext(null);
 import axios from "axios";
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-v127.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
